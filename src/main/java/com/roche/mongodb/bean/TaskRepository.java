dca1a1ba9d3197da853e980a8aecc71d53e2cd4a
Ula <ula@sample.com>
2011-06-23 21:35:32 +0200

    delete sample Task bean

--- src/main/java/com/roche/mongodb/bean/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.roche.mongodb.bean;
-
-import com.roche.mongodb.model.Task;
-import org.bson.types.ObjectId;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task, ObjectId> {
-
-}
