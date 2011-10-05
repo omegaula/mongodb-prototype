707c451540503e67026085324cf1a27340dc26a3
Ula <ukrukar@gmail.com>
2011-10-05 21:40:54 +0200

    DRY applied (composite components), editing implemented

--- src/main/java/com/roche/mongodb/dynamicforms/entries/web/EntriesController.java
+++ src/main/java/com/roche/mongodb/dynamicforms/entries/web/EntriesController.java
@@ -2,15 +2,20 @@ package com.roche.mongodb.dynamicforms.entries.web;
 
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
+import com.mongodb.RawDBObject;
 import com.roche.mongodb.dynamicforms.entries.converter.EntryConverter;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.document.mongodb.MongoTemplate;
+import org.springframework.data.document.mongodb.query.Criteria;
 import org.springframework.stereotype.Controller;
 
 import java.util.Iterator;
 import java.util.List;
 
 import static java.lang.String.format;
+import static org.springframework.data.document.mongodb.query.Criteria.where;
+import static org.springframework.data.document.mongodb.query.Query.query;
 
 @Controller("entriesCtrl")
 public class EntriesController {
@@ -18,6 +23,7 @@ public class EntriesController {
     private MongoTemplate mongoTemplate;
     private EntryConverter entryConverter;
     private List<DBObject> entries;
+    private DBObject entry;
 
     @Autowired
     public void setMongoTemplate(MongoTemplate mongoTemplate) {
@@ -34,12 +40,28 @@ public class EntriesController {
         return format("/forms/%s/list?faces-redirect=true", templateId);
     }
 
+    public String prepareEdit(String templateId, ObjectId objectId) {
+        entry = mongoTemplate.getCollection(templateId).findOne(query(where("_id").is(objectId)).getQueryObject());
+        return format("/forms/%s/edit", templateId);
+    }
+
     public String save(String templateId) {
         mongoTemplate.getCollection(templateId).save(entryConverter.convertFromRequestParams());
         return list(templateId);
     }
 
+    public String save(String templateId, ObjectId objectId) {
+        DBObject dbObject = entryConverter.convertFromRequestParams();
+        dbObject.put("_id", objectId);
+        mongoTemplate.getCollection(templateId).save(dbObject);
+        return list(templateId);
+    }
+
     public List<DBObject> getEntries() {
         return entries;
+    }
+
+    public DBObject getEntry() {
+        return entry;
     }
 }
